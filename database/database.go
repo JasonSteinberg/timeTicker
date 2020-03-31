ef9df80932dce4834ff86e0fe75e516943ee0f37
Jason Steinberg <jsteinberg555@gmail.com>
2020-03-31 11:38:42 -0600

    Ignore

--- database/database.go
+++ database/database.go
@@ -1,6 +1,24 @@
 package database
 
-func LoadDatabaseConfig(name string) {
+import (
+	"encoding/json"
+	"github.com/JasonSteinberg/timeTicker/structs"
+	"log"
+	"os"
+)
 
+func LoadDatabaseConfig(fileName string) {
+	configFile, err := os.Open(fileName)
+	defer configFile.Close()
 
+	settings := structs.DatabaseConfig{}
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&settings)
+
+	if err != nil {
+		log.Fatal(`Fatal Error: Unable to load database configuration!
+						Looking for: `, fileName)
+	}
+
+	structs.Database = settings
 }
